ca9e898fc022cbc71363c55c16b59d49d465931a
AustinKavelman <kavea9410@wrdsb.ca>
2020-10-13 13:05:31 +0000

    Finished calculating the average of the users heights.

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Calculates the average heights
+ * @author Austin Kavelman
  */
 public class Main {
 
@@ -9,7 +10,37 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //lets the user enter how many values they want
+    System.out.println("How many heights would you like to enter");
+    int amount = input.nextInt();
+
+    //create array to store the heights
+    double[] heights = new double[amount];
+
+    //use a for loop to get all the heights from the user input
+    for(int i = 0; i < heights.length; i++){
+      //ask the user for height
+      System.out.println("Enter height " + (i+1) + "(cm)");
+      //fill the array with the value
+      heights[i] = input.nextDouble();
+    }
+    
+    //create a variable to store the total of all heights
+    double total = 0;
+    //use for loop to add each value to total
+    for(int i = 0; i < heights.length; i++){
+      //add each height into total
+      total = total + heights[i];
+    }
+
+    //calculate the average height
+    double average = total/heights.length;
+
+    //let the user know the average
+    System.out.println("Your average height is " + average);
     
   }
 }
